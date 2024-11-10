17e840941d96333f31a7fae904f47de0b4ff701f
Fabio Benedetti <fabio.benedetti@vercel.com>
2024-11-10 18:09:50 +0100

    dynamic blob id

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
-
+const VERCEL_BLOB_STORE_ID = process.env.BLOB_READ_WRITE_TOKEN?.match(
+	/^vercel_blob_rw_([a-z0-9]+)_[a-z0-9]+$/i,
+)?.[1].toLowerCase();
 const nextConfig: NextConfig = {
 	images: {
 		domains: [
 			"images.ctfassets.net",
-			"nxhggsvzyhem6qpg.public.blob.vercel-storage.com",
+			`${VERCEL_BLOB_STORE_ID}.public.blob.vercel-storage.com`,
 		],
 	},
 	experimental: {
